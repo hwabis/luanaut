05a0f36d6f0cf9ce088c5f714d5ecb816ea02c23
hwabis <kennysun23@gmail.com>
2026-04-10 13:26:27 -0400

    T_T

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+### Building
+
+Install [Vulkan SDK](https://vulkan.lunarg.com/)
+
 ```
-mkdir build
-cd build
-cmake .. -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_TOOLCHAIN_FILE=/path/to/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build .
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="C:/path/to/vcpkg/scripts/buildsystems/vcpkg.cmake" -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build
 ```
--- src/luanaut/Main.cc
+++ src/luanaut/Main.cc
@@ -1,5 +1,41 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <spdlog/spdlog.h>
 #include <iostream>
 
-auto main() -> int {
-  std::cout << "???";
+int main(int argc, char* argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        spdlog::error("SDL_Init failed: {}", SDL_GetError());
+        return 1;
+    }
+
+    // Create a window with Vulkan support
+    SDL_Window* window = SDL_CreateWindow(
+        "Luanaut Engine", 
+        1280, 720, 
+        SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE
+    );
+
+    if (!window) {
+        spdlog::error("Window creation failed: {}", SDL_GetError());
+        return 1;
+    }
+
+    spdlog::info("SDL3 + Vulkan Window Initialized!");
+    std::cout << "???\n";
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+        // Vulkan Draw Loop will go here
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
