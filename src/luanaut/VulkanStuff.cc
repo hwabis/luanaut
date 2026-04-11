c6444a7a97e4cf079485436a4fd23d2d68d9730e
hwabis <kennysun23@gmail.com>
2026-04-10 22:23:44 -0400

    HOW LONG CAN THIS GO ON

--- src/luanaut/VulkanStuff.cc
+++ src/luanaut/VulkanStuff.cc
@@ -1,4 +1,5 @@
 #include "VulkanStuff.h"
+#include <vulkan/vulkan_raii.hpp>
 
 namespace luanaut {
 
@@ -8,14 +9,21 @@ constexpr bool enableValidationLayers = false;
 constexpr bool enableValidationLayers = true;
 #endif
 
+const std::vector<const char*> requiredDeviceExtension = {
+    vk::KHRSwapchainExtensionName};
+
 VulkanStuff::VulkanStuff(SDL_Window* window)
-    : instance_(createInstance()),
-      debugMessenger_(createDebugMessenger()),
+    : instance_(createInstance(context_)),
+      debugMessenger_(createDebugMessenger(instance_)),
+      physicalDevice_(createPhysicalDevice(instance_)),
+      device_(createLogicalDevice(physicalDevice_)),
+      graphicsQueue_(createGraphicsQueue(physicalDevice_, device_)),
       window_(window) {
   window_ = nullptr;  // todo rm
 }
 
-auto VulkanStuff::createInstance() -> vk::raii::Instance {
+auto VulkanStuff::createInstance(const vk::raii::Context& context)
+    -> vk::raii::Instance {
   constexpr vk::ApplicationInfo appInfo{
       .pApplicationName = "Hello Triangle",
       .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
@@ -29,7 +37,7 @@ auto VulkanStuff::createInstance() -> vk::raii::Instance {
         "VK_LAYER_KHRONOS_validation"};
     requiredLayers.assign(validationLayers.begin(), validationLayers.end());
   }
-  auto layerProperties = context_.enumerateInstanceLayerProperties();
+  auto layerProperties = context.enumerateInstanceLayerProperties();
   auto unsupportedLayerIt = std::ranges::find_if(
       requiredLayers, [&layerProperties](auto const& requiredLayer) {
         return std::ranges::none_of(
@@ -55,7 +63,7 @@ auto VulkanStuff::createInstance() -> vk::raii::Instance {
     requiredExtensions.push_back(vk::EXTDebugUtilsExtensionName);
   }
 
-  auto extensionProperties = context_.enumerateInstanceExtensionProperties();
+  auto extensionProperties = context.enumerateInstanceExtensionProperties();
   auto unsupportedPropertyIt = std::ranges::find_if(
       requiredExtensions,
       [&extensionProperties](auto const& requiredExtension) {
@@ -78,10 +86,11 @@ auto VulkanStuff::createInstance() -> vk::raii::Instance {
       .enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size()),
       .ppEnabledExtensionNames = requiredExtensions.data()};
 
-  return {context_, createInfo};
+  return {context, createInfo};
 }
 
-auto VulkanStuff::createDebugMessenger() -> vk::raii::DebugUtilsMessengerEXT {
+auto VulkanStuff::createDebugMessenger(const vk::raii::Instance& instance)
+    -> vk::raii::DebugUtilsMessengerEXT {
   if (!enableValidationLayers) {
     return nullptr;
   }
@@ -97,7 +106,7 @@ auto VulkanStuff::createDebugMessenger() -> vk::raii::DebugUtilsMessengerEXT {
       .messageSeverity = severityFlags,
       .messageType = messageTypeFlags,
       .pfnUserCallback = &debugCallback};
-  return instance_.createDebugUtilsMessengerEXT(
+  return instance.createDebugUtilsMessengerEXT(
       debugUtilsMessengerCreateInfoEXT);
 }
 
@@ -127,6 +136,99 @@ VulkanStuff::debugCallback(
   }
 
   return vk::False;
+}
+
+auto VulkanStuff::createPhysicalDevice(const vk::raii::Instance& instance)
+    -> vk::raii::PhysicalDevice {
+  auto physicalDevices = instance.enumeratePhysicalDevices();
+  auto const deviceIter =
+      std::ranges::find_if(physicalDevices, [&](auto const& physicalDevice) {
+        bool supportsVulkan1_3 =
+            physicalDevice.getProperties().apiVersion >= vk::ApiVersion13;
+
+        auto queueFamilies = physicalDevice.getQueueFamilyProperties();
+        bool supportsGraphics =
+            std::ranges::any_of(queueFamilies, [](auto const& qfp) {
+              return !!(qfp.queueFlags & vk::QueueFlagBits::eGraphics);
+            });
+
+        auto availableDeviceExtensions =
+            physicalDevice.enumerateDeviceExtensionProperties();
+        bool supportsAllRequiredExtensions = std::ranges::all_of(
+            requiredDeviceExtension,
+            [&availableDeviceExtensions](auto const& requiredDeviceExtension) {
+              return std::ranges::any_of(
+                  availableDeviceExtensions,
+                  [requiredDeviceExtension](
+                      auto const& availableDeviceExtension) {
+                    return strcmp(availableDeviceExtension.extensionName,
+                                  requiredDeviceExtension) == 0;
+                  });
+            });
+
+        auto features = physicalDevice.template getFeatures2<
+            vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan13Features,
+            vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>();
+        bool supportsRequiredFeatures =
+            features.template get<vk::PhysicalDeviceVulkan13Features>()
+                .dynamicRendering &&
+            features
+                .template get<
+                    vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>()
+                .extendedDynamicState;
+
+        return supportsVulkan1_3 && supportsGraphics &&
+               supportsAllRequiredExtensions && supportsRequiredFeatures;
+      });
+  if (deviceIter == physicalDevices.end()) {
+    throw std::runtime_error("failed to find a suitable GPU!");
+  }
+
+  return *deviceIter;
+}
+
+auto VulkanStuff::createLogicalDevice(
+    const vk::raii::PhysicalDevice& physicalDevice) -> vk::raii::Device {
+  constexpr float queuePriority = 0.5F;
+  vk::DeviceQueueCreateInfo deviceQueueCreateInfo{
+      .queueFamilyIndex = findGraphicsQueueIndex(physicalDevice),
+      .queueCount = 1,
+      .pQueuePriorities = &queuePriority};
+
+  vk::StructureChain<vk::PhysicalDeviceFeatures2,
+                     vk::PhysicalDeviceVulkan13Features,
+                     vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>
+      featureChain = {
+          {}, {.dynamicRendering = 1U}, {.extendedDynamicState = 1U}};
+
+  vk::DeviceCreateInfo deviceCreateInfo{
+      .pNext = &featureChain.get<vk::PhysicalDeviceFeatures2>(),
+      .queueCreateInfoCount = 1,
+      .pQueueCreateInfos = &deviceQueueCreateInfo,
+      .enabledExtensionCount =
+          static_cast<uint32_t>(requiredDeviceExtension.size()),
+      .ppEnabledExtensionNames = requiredDeviceExtension.data()};
+
+  return {physicalDevice, deviceCreateInfo};
+}
+
+auto VulkanStuff::createGraphicsQueue(
+    const vk::raii::PhysicalDevice& physicalDevice,
+    const vk::raii::Device& logicalDevice) -> vk::raii ::Queue {
+  return {logicalDevice, findGraphicsQueueIndex(physicalDevice), 0};
+}
+
+auto VulkanStuff::findGraphicsQueueIndex(
+    const vk::raii::PhysicalDevice& physicalDevice) -> uint32_t {
+  auto qfps = physicalDevice.getQueueFamilyProperties();
+  auto iter = std::ranges::find_if(qfps, [](auto const& qfp) {
+    return (qfp.queueFlags & vk::QueueFlagBits::eGraphics) !=
+           static_cast<vk::QueueFlags>(0);
+  });
+  if (iter == qfps.end()) {
+    throw std::runtime_error("No graphics queue family found!");
+  }
+  return static_cast<uint32_t>(std::distance(qfps.begin(), iter));
 }
 
 }  // namespace luanaut
--- src/luanaut/VulkanStuff.h
+++ src/luanaut/VulkanStuff.h
@@ -10,18 +10,32 @@ class VulkanStuff {
   VulkanStuff(SDL_Window* window);
 
  private:
-  auto createInstance() -> vk::raii::Instance;
-  auto createDebugMessenger() -> vk::raii::DebugUtilsMessengerEXT;
+  static auto createInstance(const vk::raii::Context& context)
+      -> vk::raii::Instance;
+  static auto createDebugMessenger(const vk::raii::Instance& instance)
+      -> vk::raii::DebugUtilsMessengerEXT;
   static VKAPI_ATTR vk::Bool32 VKAPI_CALL
   // NOLINTNEXTLINE(modernize-use-trailing-return-type)
   debugCallback(vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
                 vk::DebugUtilsMessageTypeFlagsEXT messageTypes,
                 const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData,
                 void* /*pUserData*/);
+  static auto createPhysicalDevice(const vk::raii::Instance& instance)
+      -> vk::raii::PhysicalDevice;
+  static auto createLogicalDevice(
+      const vk::raii::PhysicalDevice& physicalDevice) -> vk::raii::Device;
+  static auto createGraphicsQueue(
+      const vk::raii::PhysicalDevice& physicalDevice,
+      const vk::raii::Device& logicalDevice) -> vk::raii::Queue;
+  static auto findGraphicsQueueIndex(
+      const vk::raii::PhysicalDevice& physicalDevice) -> uint32_t;
 
   vk::raii::Context context_;
   vk::raii::Instance instance_ = nullptr;
   vk::raii::DebugUtilsMessengerEXT debugMessenger_ = nullptr;
+  vk::raii::PhysicalDevice physicalDevice_ = nullptr;
+  vk::raii::Device device_ = nullptr;
+  vk::raii::Queue graphicsQueue_ = nullptr;
   SDL_Window* window_;
 };
 
