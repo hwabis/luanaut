02d57d28e4bf39052ebccfa7d1340db81bab9ee4
hwabis <kennysun23@gmail.com>
2026-06-08 21:40:25 -0400

    remove unused

--- libs/lneng/include/lneng/Camera.h
+++ libs/lneng/include/lneng/Camera.h
@@ -10,14 +10,9 @@ class Camera : public Node {
 
  protected:
   auto Draw(SceneInfo& out) -> void override;
-  auto HandleEvent(const SDL_Event& event) -> bool override;
 
  private:
   float fovDeg_{};
-
-  // todo these values are completely made up until HandleEvent is first called
-  float windowWidth_ = 1;
-  float windowHeight_ = 1;
 };
 
 }  // namespace lneng
--- libs/lneng/src/Camera.cc
+++ libs/lneng/src/Camera.cc
@@ -14,15 +14,4 @@ auto Camera::Draw(SceneInfo& out) -> void {
   };
 }
 
-auto Camera::HandleEvent(const SDL_Event& event) -> bool {
-  if (event.type == SDL_EVENT_WINDOW_RESIZED) {
-    windowWidth_ = static_cast<float>(event.window.data1);
-    windowHeight_ = static_cast<float>(event.window.data2);
-
-    return true;
-  }
-
-  return false;
-}
-
 }  // namespace lneng
